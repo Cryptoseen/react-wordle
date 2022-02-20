d2b5ff988b3f8a414658545e030b9798c977a532
Cryptoseen <39794247+Cryptoseen@users.noreply.github.com>
2022-02-21 01:18:01 +0300

    update

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -38,7 +38,7 @@ export const Keyboard = ({
       } else if (e.code === 'Backspace') {
         onDelete()
       } else {
-        const key = localeAwareUpperCase(e.key)
+        const key = localeAwareUpperCase(e.key)('tr-TR')
         // TODO: check this test if the range works with non-english letters
         if (key.length === 1 && key >= 'A' && key <= 'Z') {
           onChar(key)
